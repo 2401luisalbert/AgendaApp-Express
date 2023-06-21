1824a0686f7c810864374c5bc3934804ef6583fe
2401luisalbert <125489391+2401luisalbert@users.noreply.github.com>
2023-06-21 15:45:45 -0600

    Creacion de login y logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -18,7 +18,7 @@ export const register = async (req, res) => {
         })
 
         const userSaved = await newUser.save()
-        const token = await createAccessToken({id: userSaved._id});
+        const token = await createAccessToken({ id: userSaved._id });
 
         res.cookie('token', token)
 
@@ -35,8 +35,48 @@ export const register = async (req, res) => {
 
         })
     } catch (error) {
-       res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
-export const login = (req, res) => res.send('login');
+export const login = async (req, res) => {
+    const { CURP, password } = req.body
+    try {
+
+        const userFound = await User.findOne({ CURP })
+
+        if (!userFound) return res.status(400).json({ message: "User not found" })
+
+        const isMatch = await bcrypt.compare(password, userFound.password)
+
+        if (!isMatch) return res.status(400).json({ message: "Incorrect password" })
+
+        const token = await createAccessToken({ id: userFound._id });
+
+        res.cookie('token', token)
+
+        res.json({
+            id: userFound._id,
+            name: userFound.name,
+            firstName: userFound.firstName,
+            lastName: userFound.lastName,
+            curp: userFound.CURP,
+            email: userFound.email,
+            rol: userFound.rol,
+            createAt: userFound.createdAt,
+            updateAt: userFound.updatedAt
+
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export const logout = async (req, res) => {
+    res.cookie('token', "", {
+        expires: new Date(0)
+    })
+
+    return res.sendStatus(200)
+}
+
