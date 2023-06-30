c48c5013a6a685d3c6187b632274ed90efdc9283
2401luisalbert <125489391+2401luisalbert@users.noreply.github.com>
2023-06-30 13:04:36 -0600

    Validacion de formatos de imagen

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -1,12 +1,10 @@
 import multer from "multer";
 import fs from "fs";
-import User from "../models/user.model.js";
 
 const storage = multer.diskStorage({
   destination: async function (req, file, cb) {
     try {
       const userId = req.params.id;
-      const folderUser = await User.findById(userId);
       const directory = `public/img/${userId}`;
 
       // Verificar si el directorio existe, si no, crearlo
@@ -21,9 +19,17 @@ const storage = multer.diskStorage({
     }
   },
   filename: function (req, file, cb) {
-    const ext = file.originalname.split(".").pop();
+    const ext = file.originalname.split(".").pop().toLowerCase();
+    if (ext !== "jpg" && ext !== "jpeg") {
+      return cb(new Error("Solo se permiten archivos en formato JPG o PNG."));
+    }
+
+    const destination = req.destination || ""; // Obtener el valor de req.destination si está definido, de lo contrario asignar una cadena vacía
+    console.log("destination", destination)
+
     cb(null, `${Date.now()}.${ext}`);
   },
 });
 
 export const upload = multer({ storage: storage });
+
